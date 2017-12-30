175fda0012bea8133184406351e93d1673ede7f0
PhilJungschlaeger <pjungschlaeger@web.de>
2017-12-30 12:39:02 +0100

    on

---
+++ source/time_based_video.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>            //?in/out?
+#include <opencv2/opencv.hpp> //opencv library
+#include <time.h>             //time measuring & seed
+#include <string.h>           //? <3 string ?
+#include <vector>             //storing image data
+#include <sys/types.h>        //?folder?
+#include <dirent.h>           //?folder?
+#include <errno.h>            //?folder?
+#include <iostream>           //?in/out?
+#include <math.h>             //math
+#include <image_tool.h>
+#include <video_tool.h>
+
+using namespace cv;
+
+int main(int argc, char** argv )
+{
+  /*
+  //VIDEO:
+  VideoCapture capture;   //to store the Video
+  std::cout<<"video\n";
+  capture=VideoCapture(argv[1]);    //first param: video
+  Frame_factors fac(2,100);
+  //fac.Average();
+  fac.Linear(0.1, 0);
+  Time_based_picture pic_maker(capture, fac, 1000, 1100);
+  pic_maker.render();
+  //reade input:
+  */
+}
